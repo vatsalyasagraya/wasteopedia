344f5f579480263216ea6891a833e53c42510920
vatsalyasagraya <2020uec1198@mnit.ac.in>
2023-01-31 21:04:15 +0530

    new commit

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -4,11 +4,11 @@ import "./contact.css"
 export const Contact = () => {
   return (
     <div id="Contact" className="contact">
-      <a href="mailto:kuldeep.er@gmail.com?body=Write%20your%20query%20here..."  className="btn">Contact Us</a>
+      <a href="mailto:kuldeep.er@gmail.com?body=Write%20your%20query%20here..." target="_blank" rel="noopener noreferrer" className="btn">Contact Us</a>
       <div className="footer">
         <p className="p1">Copyright © 2023</p>
         <a href="/" className="p2">linkedin</a>
-        <a href="mailto:kuldeep.er@gmail.com"  className="p3">Mail: kuldeep.er@gmail.com</a>
+        <a href="mailto:kuldeep.er@gmail.com" target="_blank" rel="noopener noreferrer" className="p3">Mail: kuldeep.er@gmail.com</a>
       </div>
     </div>
   );
